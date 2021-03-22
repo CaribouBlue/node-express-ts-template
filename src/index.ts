1dd485411c9032ddcbe5f795c855716e9e71c45d
Alec Draymore <Draymore26@gmail.com>
2021-03-22 16:43:54 -0400

    added module alias support

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 process.env["NODE_CONFIG_DIR"] = __dirname + "/config/";
 
-import { app } from './server'
+import 'module-alias/register'
+import { app } from '~/server'
 import config from 'config'
 
 const PORT: number = config.get('server.port')
